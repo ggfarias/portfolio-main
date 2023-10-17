467c0851275618b99e4803ac0898006ee8e1bfff
Gustavo Farias <gustavocgf@gmail.com>
2023-10-17 20:45:17 -0300

    Edit AboutMe

--- src/components/MainAboutMe/index.jsx
+++ src/components/MainAboutMe/index.jsx
@@ -6,7 +6,7 @@ import profilePicture from '../../assets/img-portfolio.jpeg'
 const StylesMainAboutMe = styled.div`
     .page-content {
         background: linear-gradient(var(--light), var(--light-stronger));
-        height: 100vh;
+        height: 100%;
         width: 100%;
         display: flex;
         justify-content: center;
@@ -66,8 +66,8 @@ const StylesMainAboutMe = styled.div`
     .text-content p {
         font-size: 1.5rem;
         font-weight: 400;
-        text-align: justify;
         padding-bottom: 1rem;
+        text-align: justify;
     }
 
     .text-content a {
@@ -91,6 +91,10 @@ const MainAboutMe = () => {
                         <h1>About Me</h1>
                         <p>I'm a self taught Software Developer, who seeks to solve real world problems with technology. I'm currently learning about Web Development with emphasis on Front-end, and creating responsive web apps . I haven't worked in a company as a software developer yet, but I've been studying for about 1 year and I have projects published on GitHub and on the internet.</p>
                         <p>I have previous work experience with Project Management, Team Leadership and Tech Recruiting, when I worked in a Scale-up startup for almost 3 years. Check out my <a href="https://www.linkedin.com/in/gustavo-farias29/" target='_blank'>LinkedIn</a> for more info.</p>
+
+                        <h1>Academic Background</h1>
+                        <p>My academic career began in the area of Business Administration, at the <a href="https://www.ufba.br/" target='_blank'>Federal University of Bahia (Brazil).</a> There I developed several soft skills and learned about project management. While still in college, I started working at a <a href="https://byintera.com/" target='_blank'>technology startup</a> in the project area, and got to know the area of digital products and software development.</p>
+                        <p>It was then that I realized that I would really like to work with this, within the area of Software engineering. After that, I studied for 1 year on the Systems Development and Analysis course at <a href="https://www.fiap.com.br/" target='_blank'>FIAP</a>, another renowned technology college in Brazil.</p>
                     </section>
                 </div>
             </div>
